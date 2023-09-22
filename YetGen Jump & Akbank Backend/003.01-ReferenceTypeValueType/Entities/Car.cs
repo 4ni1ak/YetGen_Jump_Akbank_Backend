3fbe2013444749961080be8fd1c4dae522196899
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-09-22 23:44:05 +0300

    ReferenceTypeValueType added

---
+++ YetGen Jump & Akbank Backend/003.01-ReferenceTypeValueType/Entities/Car.cs
@@ -0,0 +1,40 @@
+﻿using _003._01_ReferenceTypeValueType.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003._01_ReferenceTypeValueType.Entities
+{
+    public class Car
+    {
+        public string Model { get; }
+        public CarType Type { get; }
+        public bool IsRented { get; private set; }
+
+        public Car(string model, CarType type)
+        {
+            Model = model;
+            Type = type;
+            IsRented = false;
+        }
+
+        public void Rent()
+        {
+            if (!IsRented)
+            {
+                IsRented = true;
+            }
+        }
+
+        public void Return()
+        {
+            if (IsRented)
+            {
+                IsRented = false;
+            }
+        }
+    }
+}
+
