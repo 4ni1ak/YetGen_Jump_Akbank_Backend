f9b78f7ba755421ab3b606d46a516b10e98b4c3c
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-09-24 23:54:43 +0300

    EncapsulationExercise added

---
+++ YetGen Jump & Akbank Backend/004.02-EncapsulationExercise/Entities/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004._02_EncapsulationExercise.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname{ get; set; }
+        private decimal balance { get; set; }
+        public decimal Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (Math.Abs(value - balance) <=500 )
+                {
+                    balance = value;
+                    Console.WriteLine($"Current value {balance}");
+
+                }
+            }
+        }
+
+        public Account(string name, string surname)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            Balance = 2000;
+        }
+      
+
+    }
+}
---
+++ YetGen Jump & Akbank Backend/004.02-EncapsulationExercise/Program.cs
@@ -0,0 +1,6 @@
+﻿using _004._02_EncapsulationExercise.Entities;
+
+Account account1 = new("Anil", "Akpinar");
+account1.Balance -= 100;
+account1.Balance += 10000000;
+Console.WriteLine($"Current value {account1.Balance}");
