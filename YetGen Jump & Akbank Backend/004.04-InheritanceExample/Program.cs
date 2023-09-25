1d20bff422f645cad1e5159efd868c8a27af907c
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-09-25 14:47:53 +0300

    InheritanceExample added

---
+++ YetGen Jump & Akbank Backend/004.04-InheritanceExample/Program.cs
@@ -0,0 +1,20 @@
+﻿BaseClass baseClass = new();
+baseClass.Text = "Test";
+baseClass.Print();
+DerivedClass derivedClass = new();
+derivedClass.Text = "Test 2";
+derivedClass.Print();
+class BaseClass //Parent Kalıtım Veren 1
+{
+
+    public string Text { get; set; }
+
+    public void Print()
+    {
+        Console.WriteLine("Print Method");
+    }
+}
+class DerivedClass: BaseClass //Child 1 Kalıtım Alan
+{
+    public int Number { get; set; }
+}
