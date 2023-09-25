c29921349270a6385303b1c3bbd69b6e56965ba0
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-09-25 14:48:22 +0300

    CalendarApp added

---
+++ YetGen Jump & Akbank Backend/004.05-CalendarApp/Entities/Meeting.cs
@@ -0,0 +1,14 @@
+﻿using _004._05_CalendarApp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004._05_CalendarApp.Entities
+{
+    internal class Meeting:Event 
+    {
+        public List<string> Guests { get; set; }      
+    }
+}   
