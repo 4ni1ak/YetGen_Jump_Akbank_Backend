6c85cdcb7a02e9a5f483cea31531ab4e83b745e3
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-09-25 14:48:56 +0300

    InterfaceExample added

---
+++ YetGen Jump & Akbank Backend/004.06-InterfaceExample/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Interfaces!");
+SamsungPrinter samsungPrinter = new();
+samsungPrinter.PrintPage();
+HPPrinter hpPrinter = new();
+hpPrinter.PrintPage();
+interface IPrinter
+{
+    void PrintPage();
+}
+class SamsungPrinter: IPrinter
+{
+    public void PrintPage()
+    {
+       Console.WriteLine("Printing in Black and White");
+    }
+
+}
+
+class HPPrinter:IPrinter
+{
+    public void PrintPage()
+    {
+        Console.WriteLine("Printing in Color");
+
+    }
+}
