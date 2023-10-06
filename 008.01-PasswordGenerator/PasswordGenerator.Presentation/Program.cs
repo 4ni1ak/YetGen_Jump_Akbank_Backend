aa009a5880fb39c4ea68fe61795efa150f148615
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-06 18:54:33 +0300

    PasswordGenerator added

---
+++ 008.01-PasswordGenerator/PasswordGenerator.Presentation/Program.cs
@@ -0,0 +1,29 @@
+﻿using Yetgen.PWGenerator;
+
+var passwordGenerator = new PasswordGenerator();
+
+Console.WriteLine("Please enter the desired length for your password.");
+
+var passwordLength = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Should your password include numbers?");
+
+var includeNumbers = Console.ReadLine() == "yes";
+
+Console.WriteLine("Should your password include lowercase characters?");
+
+var includeLowerCase = Console.ReadLine() == "yes";
+
+Console.WriteLine("Should your password include uppercase characters?");
+
+var includeUpperCase = Console.ReadLine() == "yes";
+
+Console.WriteLine("Should your password include special characters?");
+
+var includeSpecialChars = Console.ReadLine() == "yes";
+
+var password = passwordGenerator.Generate(passwordLength, includeNumbers, includeLowerCase, includeUpperCase, includeSpecialChars);
+
+Console.WriteLine($"Your password: {password}");
+
+Console.ReadLine();
