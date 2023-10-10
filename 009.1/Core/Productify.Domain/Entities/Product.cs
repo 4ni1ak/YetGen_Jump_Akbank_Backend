ff747932f197438bc2a561f191b2726c992a227a
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-10 23:16:27 +0300

    ProductifyMVC added (10.10.2023)

---
+++ 009.1/Core/Productify.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Productify.Domain
+{
+	public class Product
+	{
+	
+		public Guid Id{ get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedOn{ get; set; }
+		public Product( string name)
+		{
+			Id = Guid.NewGuid();
+			Name = name;
+			CreatedOn = DateTime.Now;
+		
+		}
+
+	}
+}
