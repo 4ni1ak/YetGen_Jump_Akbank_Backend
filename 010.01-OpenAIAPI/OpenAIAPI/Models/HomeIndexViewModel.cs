7c875d62d76d56b273793270d2a445b89c6b34d8
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-15 21:52:21 +0300

    OpenAIAPI added (15.10.2023)

---
+++ 010.01-OpenAIAPI/OpenAIAPI/Models/HomeIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenAIAPI.Models
+{
+    public class HomeIndexViewModel
+    {
+       
+        public string Prompt{ get; set; }
+        public int ImageCount { get; set; }
+        public List<string> ImageUrl { get; set; }
+        public HomeIndexViewModel()
+        {
+            ImageUrl = new List<string>();
+        }
+
+    }
+}
