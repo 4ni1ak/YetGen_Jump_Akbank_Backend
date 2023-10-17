1013efd33e35b32a0624db434e5cdf54bdf01ad5
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-17 17:53:56 +0300

    MusicAlayShop added (17.10.2023)

---
+++ 011.01-Redro/Infrastructure/RedoMusic.Persistence/Contexts/RedoMusicDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedoMusic.Domain.Entities;
+using RedoMusic.Persistence.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedoMusic.Persistence.Contexts
+{
+    public class RedoMusicDbContext : DbContext
+    {
+
+        public DbSet<Instrument> Instruments { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configurations.GetString("ConnectionStrings:PostgreSQL"));
+        }
+
+        
+    }
+
+}
